7f9494089467e1490291581c03a8a200fdf4652b
Lucas <88948009+Luskario@users.noreply.github.com>
2022-06-05 20:50:34 -0300

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "metodos.h"
+#include "sqrtsort.h"
+
+int main (void){
+
+    int tam[4] = {10000, 100000, 1000000, 10000000};
+    int i, c, vezes=1;
+    clock_t timer;
+    float temp_m[8];
+    int* vetor = (int*)malloc(10000000*sizeof(int));
+    //int vetor[1000000];
+    int* aux = (int*)malloc(10000000*sizeof(int));
+
+    for(i=0; i<1; i++){
+
+        gerarVetor(vetor, 1000000);
+
+        timer = clock();
+        sqrByBubble(vetor, 1000000);
+        timer = clock() - timer;
+
+        temp_m[0] = ((float)timer/CLOCKS_PER_SEC);
+        temp_m[0] = (temp_m[0] + temp_m[i]*1000)/vezes;
+
+        
+
+    }
+
+    printf("%.5f\n", temp_m[0]);
+    
+    free(aux);
+    free(vetor);
+    
+
+    return 0;
+}
