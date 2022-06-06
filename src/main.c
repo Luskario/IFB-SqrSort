f8a32236f278cade3a061b44e27697670541ed0f
Lucas <88948009+Luskario@users.noreply.github.com>
2022-06-06 11:03:52 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,32 +8,40 @@ int main (void){
 
     int tam[4] = {10000, 100000, 1000000, 10000000};
     int i, c, vezes=1;
-    clock_t timer;
-    float temp_m[8];
-    int* vetor = (int*)malloc(10000000*sizeof(int));
+    //clock_t timer;
+    //float temp_m[8] = {0, 0, 0, 0, 0, 0, 0, 0}, temp_aux;
+    int teste[10] = {76, 18, 1, 43, 42, 2, 5, 7, 9, 10};
+    //int* vetor = (int*)malloc(10000000*sizeof(int));
     //int vetor[1000000];
-    int* aux = (int*)malloc(10000000*sizeof(int));
+    //int* aux = (int*)malloc(10000000*sizeof(int));
 
-    for(i=0; i<1; i++){
+    //for(i=0; i<1; i++){
 
-        gerarVetor(vetor, 1000000);
+        //gerarVetor(vetor, 1000000);
 
-        timer = clock();
-        sqrByBubble(vetor, 1000000);
-        timer = clock() - timer;
+        //----------------------------------------------------------------//
+        //timer = clock();
+        //aux = sqrByBubble(vetor, 1000000);
+        //timer = clock() - timer;
 
-        temp_m[0] = ((float)timer/CLOCKS_PER_SEC);
-        temp_m[0] = (temp_m[0] + temp_m[i]*1000)/vezes;
+        //temp_aux = ((float)timer/CLOCKS_PER_SEC);
+        //temp_m[0] = (temp_m[0] + temp_m[i]*1000)/vezes;
 
-        
+        //----------------------------------------------------------------//
 
+    //}
+    
+    makeHeap(teste, 10);
+    for(i=0; i<10; i++){
+        printf("%d - ", teste[i]);
     }
 
-    printf("%.5f\n", temp_m[0]);
+    //printf("%.5f\n", temp_m[0]);
     
-    free(aux);
-    free(vetor);
+    //free(aux);
+    //free(vetor);
     
 
     return 0;
 }
+
