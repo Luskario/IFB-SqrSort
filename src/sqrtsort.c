7f9a4ffed9a6693bfb2025801aae97fc4455ffd3
Lucas <88948009+Luskario@users.noreply.github.com>
2022-06-06 11:06:13 -0300

    Update sqrtsort.c

--- src/sqrtsort.c
+++ src/sqrtsort.c
@@ -59,22 +59,37 @@ int* sqrByHeap(int *V, int tam_total){
 		aux2 = tam_part*aux1;
 		makeHeap(V, aux2, tam_part, tam_total);
 	}
-    //---------------------------------------------------------------------------------------------------//
-	Dado Vmaiores[tam_part];
+	
+	Dado *Vmaiores = (Dado*) malloc(10000*sizeof(Dado));
 	int *Vnew = calloc(tam_total, sizeof(int));
 	int maior = -999999999;
 
 	for(aux1 = 0; aux1 < n_part; aux1++) {			// seleciona os maiores de cada parte
 		aux2 = tam_part*aux1;
-		Vmaiores[aux1].valor = V[aux2];
+		Vmaiores[aux1].valor = V[aux2];for(aux1=0; aux1<n_part; aux1++) {
+		    if(Vmaiores[aux1].valor > maior) {
+				maior = Vmaiores[aux1].valor;
+				aux2 = aux1;
+			}
+		}
+		Vnew[n] = maior;
+        Vmaiores[aux2].indice++;
+        Vmaiores[aux2].percorridos++;
+
+		if(Vmaiores[aux2].percorridos < tam_part && Vmaiores[aux2].indice < tam_total) {
+			Vmaiores[aux2].valor = V[Vmaiores[aux2].indice];
+		} else {
+			Vmaiores[aux2].valor = -999999999;
+		}
+	    maior = -999999999;
         Vmaiores[aux1].indice = aux2;
         Vmaiores[aux1].percorridos = 0;
-		//printf("%d -- %d - %d - %d \n", aux1, Vmaiores[aux1].indice, Vmaiores[aux1].valor, Vmaiores[aux1].percorridos);
 	}
 		
 	for(n=tam_total-1; n>=0; n--) {						//ordena os maiores valores no novo vetor ordenado
-	
+		
 	}
+	free(Vmaiores);
 	return Vnew;
 }
 
